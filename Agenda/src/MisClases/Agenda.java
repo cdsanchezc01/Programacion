8b6ad8381820b961277e4a2c34b50c535b6c5405
cdsanchezc01 <cdsanchezc01@educarex.es>
2023-11-17 14:34:59 +0100

    17/11/2023

--- Agenda/src/MisClases/Agenda.java
+++ Agenda/src/MisClases/Agenda.java
@@ -1,5 +1,56 @@
 package MisClases;
 
-public class Agenda {
+import java.util.Scanner;
 
+public class Agenda {
+//	ATRIBUTOS
+	DatosPersona[] agenda;
+	
+	
+	Agenda(int numEntradas){
+		
+		agenda=new DatosPersona[numEntradas];
+	}
+	
+	
+	int insertarEntrada() {
+		
+//		BUSCAMOS EL PRIMER HUECO VACIO EN LA AGENDA
+		for (int i=0; i<agenda.length; i++) {
+			if (agenda[i]==null) {
+				break;
+			}
+		}
+		if(encontrado==true) {
+//			HE ECONTRADO EL ELEMENTO
+//			LE PEDIMOS AL USUARIO LOS DATOS DEL NUEVO REGISTRO
+			Scanner teclado=new Scanner(System.in);
+			
+			System.out.println("Introduce el nombre");
+			String nombre=teclado.nextLine();
+			System.out.println("Introduce la direccion");
+			String direccion=teclado.nextLine();
+			System.out.println("Introduce el número fijo");
+			String fijo=teclado.nextLine();
+			System.out.println("Introduce el número móvil");
+			String móvil=teclado.nextLine();
+			System.out.println("Introduce el email");
+			String eMail=teclado.nextLine();
+			
+//			CREAMOS UN OBJETO DE LA CLASE DatosPersona
+			DatosPersona nuevoRegistro=new DatosPersona(nombre,direccion,fijo,movil,eMail);
+			
+//			INSERTO EL OBJETO EN LA POSICION BUSCADA EN LA AGENDA
+			agenda[i]=nuevoRegistro;
+			
+			
+		}
+		else {
+//			LA AGENDA ESTABA LLENA 
+		}
+		
+	}
+	
+	
+//	METODOS
 }
--- Agenda/src/MisClases/DatosPersona.java
+++ Agenda/src/MisClases/DatosPersona.java
@@ -1,5 +1,20 @@
 package MisClases;
 
 public class DatosPersona {
+//	ATRIBUTOS
+	String nombre;
+	String direccion;
+	String numFijo;
+	String numMovil;
+	String eMail;
+	
+	DatosPersonas(String nom, String dir,String fijo,String movil,String eMail,){
+		nombre=nom;
+		direccion=dir;
+		numFijo=fijo;
+		numMovil=movil;
+		eMail=mail;
+	}
+	
 
 }
